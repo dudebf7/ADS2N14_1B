42c58a3a60e14a9f1414800648f5d608a87c97b4
Eduardo da Silva Luiz <dude_bf7@hotmail.com>
2014-02-23 21:49:58 -0300

    HelloWorld

---
+++ HelloWorld/HelloYou.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class HelloYou {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		        Scanner HelloYou = new Scanner(System.in);
+		        String nome;
+		        
+		        System.out.println("Digite o seu usuário: ");
+		        nome = HelloYou.next();
+		        
+		        System.out.printf("Hello " + nome + "\n");
+		
+	
+	}
+
+}
